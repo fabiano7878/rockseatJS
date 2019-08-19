f8bc67179fab0fdb786ad51683696eb21cbf5367
fabiano7878 <fabiano.asistemas@gmail.com>
2019-08-18 21:21:39 -0300

    implemntação de listas

--- JavaScript/todos.js
+++ JavaScript/todos.js
@@ -1,12 +1,43 @@
-var containerLement = document.querySelector('.todos');
+var containerElement = document.querySelector('.todos');
 
 var listaTodos = document.createElement('ul');
 
-var lFazerCafe = document.createComment('li');
-var lEstudarJs = document.createComment('li');
-var lAcessoComunidade = document.createComment('li');
+var lFazerCafe = document.createElement('li');
+var lEstudarJs = document.createElement('li');
+var lAcessoComunidade = document.createElement('li');
 
+
+linhaFazerCafe();
+linhaEstudar();
+
+//Adicionando Linhas a Lista
 listaTodos.appendChild(lFazerCafe);
 listaTodos.appendChild(lEstudarJs);
+listaTodos.appendChild(lAcessoComunidade);
 
-containerLement.appendChild(listaTodos);
+//Adicionando a Lista na Div 
+containerElement.appendChild(listaTodos);
+
+//Linha "Fazer Café" tratando a linha do Café, adicionando o link "excluir".
+function linhaFazerCafe() {
+    var txtFazerCafe = document.createTextNode("Fazer caf\u00E9 - ");
+    var linkCafe = document.createElement('a');
+    linkCafe.setAttribute('href', '#');
+    var txtExcluirCafe = document.createTextNode('excluir');
+    linkCafe.appendChild(txtExcluirCafe);
+    lFazerCafe.setAttribute('id', 'cafe');
+    lFazerCafe.appendChild(txtFazerCafe);
+    lFazerCafe.appendChild(linkCafe);
+}
+
+//Linha "Fazer Café" tratando a linha do Café, adicionando o link "excluir".
+function linhaEstudar() {
+    var txtEstudar = document.createTextNode("Estudar JS - ");
+    var linkEstudar = document.createElement('a');
+    linkEstudar.setAttribute('href', '#');
+    var txtExcluirEstudar = document.createTextNode('excluir');
+    linkEstudar.appendChild(txtExcluirEstudar);
+    lEstudarJs.setAttribute('id', 'estudar');
+    lEstudarJs.appendChild(txtEstudar);
+    lEstudarJs.appendChild(linkEstudar);
+}
