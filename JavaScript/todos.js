48603987f0344e1ece416185a0dbf380538aea66
Fabiano Dos Santos Silva <fabsilva@scopus.com.br>
2019-08-20 14:39:56 -0300

    teste implementação

--- JavaScript/todos.js
+++ JavaScript/todos.js
@@ -2,9 +2,9 @@ var containerElement = document.querySelector('.todos');
 
 var listaTodos = document.createElement('ul');
 
-var lFazerCafe = document.createElement('li');
-var lEstudarJs = document.createElement('li');
-var lAcessoComunidade = document.createElement('li');
+//var lFazerCafe = document.createElement('li');
+//var lEstudarJs = document.createElement('li');
+//var lAcessoComunidade = document.createElement('li');
 
 var campoNovaLinha = document.createElement('input');
 var btnAdicionaLinha = document.createElement('button');
@@ -18,21 +18,23 @@ campoNovaLinha.setAttribute('placeholder','Nome da Linha');
 btnAdicionaLinha.setAttribute('id','addlinha');
 btnAdicionaLinha.appendChild(txtBtnAdicionaLinha);
 
-linhaFazerCafe();
-linhaEstudar();
+//linhaFazerCafe();
+//linhaEstudar();
+//linhaComunidade();
+
 
 //Adicionando Linhas a Lista
-listaTodos.appendChild(lFazerCafe);
-listaTodos.appendChild(lEstudarJs);
-listaTodos.appendChild(lAcessoComunidade);
-listaTodos.appendChild(campoNovaLinha);
-listaTodos.appendChild(btnAdicionaLinha);
+//listaTodos.appendChild(lFazerCafe);
+//listaTodos.appendChild(lEstudarJs);
+//listaTodos.appendChild(lAcessoComunidade);
 
 //Adicionando a Lista na Div 
 containerElement.appendChild(listaTodos);
+containerElement.appendChild(campoNovaLinha);
+containerElement.appendChild(btnAdicionaLinha);
 
 //Linha "Fazer Café" tratando a linha do Café, adicionando o link "excluir".
-function linhaFazerCafe() {
+/** function linhaFazerCafe() {
     var txtFazerCafe = document.createTextNode("Fazer caf\u00E9 - ");
     var linkCafe = document.createElement('a');
     linkCafe.setAttribute('href', '#');
@@ -41,10 +43,10 @@ function linhaFazerCafe() {
     lFazerCafe.setAttribute('id', 'cafe');
     lFazerCafe.appendChild(txtFazerCafe);
     lFazerCafe.appendChild(linkCafe);
-}
+}*/
 
-//Linha "Fazer Café" tratando a linha do Café, adicionando o link "excluir".
-function linhaEstudar() {
+//Linha "Estudar" tratando a linha Estudar, adicionando o link "excluir".
+/**function linhaEstudar() {
     var txtEstudar = document.createTextNode("Estudar JS - ");
     var linkEstudar = document.createElement('a');
     linkEstudar.setAttribute('href', '#');
@@ -53,41 +55,47 @@ function linhaEstudar() {
     lEstudarJs.setAttribute('id', 'estudar');
     lEstudarJs.appendChild(txtEstudar);
     lEstudarJs.appendChild(linkEstudar);
-}
+}*/
 
+//Linha "Acesso a Comunidade" tratando a linha do Café, adicionando o link "excluir".
+/**function linhaComunidade() {
+    var txtComunidade = document.createTextNode("Comunidade - ");
+    var linkComunidade = document.createElement('a');
+    linkComunidade.setAttribute('href', '#');
+    var txtExcluirComunidade = document.createTextNode('excluir');
+    linkComunidade.appendChild(txtExcluirComunidade);
+    lAcessoComunidade.setAttribute('id', 'comunidade');
+    lAcessoComunidade.appendChild(txtComunidade);
+    lAcessoComunidade.appendChild(linkComunidade);
+}*/
 
 
 
 btnAdicionaLinha.onclick = function(){                
-    var todos = ['Fazer café', 'Estudar','Comunidade'];    
+    //var todos = ['Fazer café', 'Estudar','Comunidade'];
+    var todos = [];
     var pnovaLinha = document.querySelector('#nomelinha').value;
 
     if(pnovaLinha === null || pnovaLinha === ''){
         alert('Adicione o nome da linha!');
     }else{
-        addNovaLinha(pnovaLinha, todos);
-    }
-
-    console.log('1: '+todos);
+        atualizaLista(pnovaLinha, todos);
+    }        
     for(todo of todos){
-        console.log('T: '+todo);
+      console.log('T: '+todo);
+        novaLinha();
     }
 }
-function addNovaLinha(nome, todos) {    
-    console.log('Parametros nome: '+nome+' todos: '+todos);
-    console.log('tamanho: '+todos.length);
-    for(i = 0; i < todos.length; i++){        
-        console.log('i: '+i+' todos: '+todos.length);
-        if(todos.length == 0){
-            console.log(' = 0');
+function atualizaLista(nome, todos) {    
+     for(i = 0; i <= todos.length; i++){         
+        if(todos.length == 0){            
            todos[i] = nome;
         }
-        if(i == (todos.length - 1)){
-           console.log(' =! 0 ');
+        if(i > 0 && i == (todos.length - 1)){           
            todos[i+1] = nome;
            break; 
         }               
-    }
-    console.log('tamanho: '+todos.length);
+    }    
 }
+
 
