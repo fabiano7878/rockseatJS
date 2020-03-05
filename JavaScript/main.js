6f639d5f5457e82c888a6ed39d7c03f1e4317f84
Fabiano Dos Santos Silva <fabsilva@scopus.com.br>
2020-03-05 16:37:20 -0300

    implementando exercicios aula 4

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,8 +1,9 @@
-//Versão sem a api AXIO.
+//Versão sem a api AXIOS.
+/**
 var minhaPromise = function(){
     return new Promise(function(resolve, reject){
-        var xhr =new XMLHttpRequest();
-        xhr.open('GET', 'https://api.github.com/users/afoaline');
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://api.github.com/users/fabiano7878');
         xhr.send(null);
 
         xhr.onreadystatechange = function(){
@@ -18,6 +19,59 @@ var minhaPromise = function(){
 }
 
 minhaPromise()
+    .then(function(reponse){
+        console.log(reponse);
+    })
+    .catch(function(error){
+        console.warn(error);
+    });
+ */
+
+ //usando a api AXIOS para fazer a mesma coisa do codigo acima, usar o ajax e a promise.
+ axios.get('https://api.github.com/users/fabiano7878')
+    .then(function(reponse){
+        console.log(reponse);
+    })
+    .catch(function(error){
+        console.warn(error);
+    });
+
+
+
+
+/** Exercicios da Aula 4
+ * Criar função que verifica se usuer é maior que 18 anos, usar promises.
+*/
+
+//Criando os Elementos, botão, campo texto e texto para botão.
+var campoValidaIdade = document.createElement('input');
+var btnValidaIdade = document.createElement('button');
+var txtBtnValidaIdade = document.createTextNode('Idade');
+
+//Atribuindo valores para o elemento Campo de Texto
+campoValidaIdade.setAttribute('type','text');
+campoValidaIdade.setAttribute('id', 'validaIdade');
+campoValidaIdade.setAttribute('name', 'validaIdade');
+campoValidaIdade.setAttribute('placeholder','Validar Idade Usuário');
+
+//atribuindo informações ao botão.
+btnValidaIdade.setAttribute('id','validaIdade');
+btnValidaIdade.appendChild(txtBtnValidaIdade);
+
+//adicionando o botão e o campo de texto na TextTrackList.
+containerElement.appendChild(campoValidaIdade);
+containerElement.appendChild(btnValidaIdade);
+
+
+btnValidaIdade.onclick = function(idade){
+    var promiseValidarIdade = function(){    
+        return new Promise(function(resolve, reject){
+            
+        });
+    }   
+}
+
+promiseValidarIdade()
     .then(function(reponse){
         console.log(reponse);
     })
